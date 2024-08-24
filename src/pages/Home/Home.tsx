2595007db8df53b4b9e99a6398989aa57a95c2fc
Saim Al Ifran <phpmysql811@gmail.com>
2024-08-24 16:22:10 +0600

    lastest event section added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './sections/Banner'
 import EventCategories from './sections/EventCategories'
+import EventsSection from './sections/EventSection'
 
 const Home:React.FC = () => {
   return (
     <>
        <Banner/>
        <EventCategories/>
+       <EventsSection/>
     </>
   )
 }
